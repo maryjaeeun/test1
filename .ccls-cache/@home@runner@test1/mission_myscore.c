e7802224c353d267ebd70ad0c3b204f4d5bf0f14
maryjaeeun <128555448+maryjaeeun@users.noreply.github.com>
2023-05-24 00:23:56 +0000

    while문

---
+++ .ccls-cache/@home@runner@test1/mission_myscore.c
@@ -0,0 +1,18 @@
+// #include<stdio.h>
+// int main(){
+//   int a;
+//   printf("성적이 어떻게 되시나요?\n");
+//   scanf("%d", &a);
+
+//   if(a>=90)
+//     printf("A학점");
+//   else if(a>=80)
+//     printf("B학점");
+//   else if(a>=70)
+//     printf("C학점");
+//   else if(a>=60)
+//     printf("D학점");
+//   else
+//     printf("F학점");
+//   printf("수고하셨습니다.");
+// }
---
+++ mission1.c
@@ -0,0 +1,11 @@
+// #include <stdio.h>
+// main(){
+//   int x;
+//  printf("정수 입력 : ");
+//   scanf("%d", &x);
+
+//   if(x%2==4)
+//     printf("짝수");
+//   else
+//     printf("홀수");
+// }
